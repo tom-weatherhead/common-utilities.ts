36d5c0d7ce90b906a9559db7e6eed1182fe8b87b
Tom Weatherhead <tomw@2hrd4u.org>
2021-11-16 18:46:48 -0700

    Dev

---
+++ src/huffman.ts
@@ -0,0 +1,69 @@
+// github:tom-weatherhead/common-utilities.ts/src/huffman.ts
+
+import { PriorityQueue } from './collection-classes/priority-queue';
+
+export interface IHuffmanEncodingTreeNode<T> {
+	readonly datum?: T;
+	readonly count: number;
+	readonly leftChild?: IHuffmanEncodingTreeNode<T>;
+	readonly rightChild?: IHuffmanEncodingTreeNode<T>;
+}
+
+function treeTraversal<T>(
+	treeNode: IHuffmanEncodingTreeNode<T>,
+	result: [T, string][],
+	bitString: string
+): [T, string][] {
+	if (typeof treeNode.datum !== 'undefined') {
+		result.push([treeNode.datum, bitString]);
+
+		if (
+			typeof treeNode.leftChild === 'undefined' ||
+			typeof treeNode.rightChild === 'undefined'
+		) {
+			throw new Error('HuffmanEncoding treeTraversal: Children should be undefined.');
+		}
+	} else if (
+		typeof treeNode.leftChild !== 'undefined' &&
+		typeof treeNode.rightChild !== 'undefined'
+	) {
+		treeTraversal(treeNode.leftChild, result, bitString + '0');
+		treeTraversal(treeNode.rightChild, result, bitString + '1');
+	} else {
+		throw new Error('HuffmanEncoding treeTraversal: Too many undefineds.');
+	}
+
+	return result;
+}
+
+export function createHuffmanEncoding<T>(input: Iterable<[T, number]>): [T, string][] {
+	const fnComparator = (t1: IHuffmanEncodingTreeNode<T>, t2: IHuffmanEncodingTreeNode<T>) =>
+		t1.count < t2.count;
+	const iterable = Array.from(input).map(([datum, count]: [T, number]) => {
+		return { datum, count };
+	});
+	const pq = new PriorityQueue<IHuffmanEncodingTreeNode<T>>(fnComparator, iterable);
+
+	while (pq.size > 1) {
+		const t1 = pq.dequeue();
+		const t2 = pq.dequeue();
+
+		if (typeof t1 === 'undefined' || typeof t2 === 'undefined') {
+			throw new Error('createHuffmanEncoding() : t1 or t2 is undefined.');
+		}
+
+		pq.enqueue({
+			count: t1.count + t2.count,
+			leftChild: t1,
+			rightChild: t2
+		});
+	}
+
+	const t = pq.dequeue();
+
+	if (typeof t === 'undefined') {
+		throw new Error('createHuffmanEncoding() : t is undefined.');
+	}
+
+	return treeTraversal(t, [], '');
+}
